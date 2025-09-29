34b3a59a96f9651a051203e828084b970b2c050d
yarazaytseva-lgtm <yara.zaytseva@gmail.com>
2025-09-30 00:34:02 +0300

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -1,12 +1,35 @@
+import pathlib
 import pandas as pd
-import requests
-from io import StringIO
+from data_convert import enforce_schema
 
-FILE_ID = "10eufblSIFd0USk8-6YX2O5NyMI1RlaP-"
-file_url = f"https://drive.google.com/uc?export=download&id={FILE_ID}"
+DATA_DIR = pathlib.Path("data")
+DATA_DIR.mkdir(parents=True, exist_ok=True)
 
-response = requests.get(file_url)
-raw_data = pd.read_csv(StringIO(response.text))
-print(raw_data.head(10))
+def load_raw() -> pd.DataFrame:
+    src = r"C:\Users\Mr.Chip\Desktop\DE sea\data\raw.csv"
+    df = pd.read_csv(src, encoding="utf-8", sep=",", na_values=["", "NA", "NaN", "null"], low_memory=False)
+    return df
 
-# https://drive.google.com/file/d/10eufblSIFd0USk8-6YX2O5NyMI1RlaP-/view?usp=drive_link
+
+def save(df: pd.DataFrame):
+    (DATA_DIR / "clean.csv").write_text("", encoding="utf-8")  # гарантируем путь
+    df.to_csv(DATA_DIR / "clean.csv", index=False, encoding="utf-8")
+    try:
+        df.to_parquet(DATA_DIR / "clean.parquet", index=False)
+    except Exception as e:
+        print("Parquet не сохранился:", e)
+
+def main():
+    
+    with pd.option_context("display.max_rows", None, "display.max_columns", None):
+        df = load_raw()
+        print("До:\n", df.dtypes)
+        df=enforce_schema(df)
+        
+        print("После:\n", df.dtypes)
+        save(df)
+
+
+if __name__ == "__main__":
+    main()
+
